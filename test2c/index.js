9f7187a9f3be21d498e9fa957e22af312e839c0b
whitelizard <esbblo@gmail.com>
2018-02-18 23:26:12 +0100

    optimise

--- test2c/index.js
+++ test2c/index.js
@@ -1,30 +1,16 @@
 import './style';
 import { Component } from 'preact';
-// import * as Sprite3D from './sprite3d';
-// import { spriteBox, Sprite3D } from '../';
 
 // We can "compile" a finished 3d world by copying the element html code from the browser
 // and turning transforms into matrixes with https://meyerweb.com/eric/tools/matrix/
 // Also, we can animate with css variables
 
-const uPos = 130;
-
 export class World3d extends Component {
   goDown = () => {
-    // this.box.forEach(b => b.y(uPos + 130).update());
-    console.log(this.moving.children);
-    // const len = this.moving.children.length;
     this.moving.style.transform = 'translate3d(0px, 130px, 0px)';
-    // for (let i = 0; i < len; ++i) {
-    //   this.moving.children[i].style.setProperty(
-    //     '--transform',
-    //     'matrix3d( 0.866025, 0.103956, 0.489074, 0, 0, 0.978148, -0.207912, 0, -0.5, 0.180057, 0.847101, 0, 140, 180, -300, 1)'
-    //   );
-    // }
   };
   goUp = () => {
     this.moving.style.transform = 'translate3d(0px, 0px, 0px)';
-    // this.box.forEach(b => b.y(uPos).update());
   };
   componentDidMount() {}
   shouldComponentUpdate({ pressed }) {
@@ -63,7 +49,7 @@ export class World3d extends Component {
             class="face frontface"
             style="transform: translate3d(-19.5px, -100px, 70px) rotateX(0deg) rotateY(0deg) rotateZ(0deg); width: 39px; height: 200px; background: rgb(68, 68, 68);"
           />
-          <div
+          {/*<div
             class="face backface"
             style="transform: translate3d(-19.5px, -100px, -70px) rotateX(0deg) rotateY(180deg) rotateZ(0deg); width: 39px; height: 200px; background: rgb(54, 54, 54);"
           />
@@ -74,7 +60,7 @@ export class World3d extends Component {
           <div
             class="face leftface"
             style="transform: translate3d(-89.5px, -100px, 0px) rotateX(0deg) rotateY(90deg) rotateZ(0deg); width: 140px; height: 200px; background: rgb(62, 62, 62);"
-          />
+          />*/}
           <div
             class="face topface"
             style="transform: translate3d(-19.5px, -170px, 0px) rotateX(-90deg) rotateY(0deg) rotateZ(0deg); width: 39px; height: 140px; background: rgb(74, 74, 74);"
@@ -92,14 +78,14 @@ export class World3d extends Component {
             class="face frontface"
             style="transform: translate3d(-19.5px, -100px, 70px) rotateX(0deg) rotateY(0deg) rotateZ(0deg); width: 39px; height: 200px; background: rgb(68, 68, 68);"
           />
-          <div
+          {/*<div
             class="face backface"
             style="transform: translate3d(-19.5px, -100px, -70px) rotateX(0deg) rotateY(180deg) rotateZ(0deg); width: 39px; height: 200px; background: rgb(54, 54, 54);"
           />
           <div
             class="face rightface"
             style="transform: translate3d(-50.5px, -100px, 0px) rotateX(0deg) rotateY(-90deg) rotateZ(0deg); width: 140px; height: 200px; background: rgb(63, 63, 63);"
-          />
+          />*/}
           <div
             class="face leftface"
             style="transform: translate3d(-89.5px, -100px, 0px) rotateX(0deg) rotateY(90deg) rotateZ(0deg); width: 140px; height: 200px; background: rgb(62, 62, 62);"
@@ -121,10 +107,10 @@ export class World3d extends Component {
             class="face frontface"
             style="transform: translate3d(-19.5px, -100px, 70px) rotateX(0deg) rotateY(0deg) rotateZ(0deg); width: 39px; height: 200px; background: rgb(68, 68, 68); border-radius: 2px"
           />
-          <div
+          {/*<div
             class="face backface"
             style="transform: translate3d(-19.5px, -100px, -70px) rotateX(0deg) rotateY(180deg) rotateZ(0deg); width: 39px; height: 200px; background: rgb(54, 54, 54);"
-          />
+          />*/}
           <div
             class="face rightface"
             style="transform: translate3d(-50.5px, -100px, 0px) rotateX(0deg) rotateY(-90deg) rotateZ(0deg); width: 140px; height: 200px; background: rgb(63, 63, 63);"
@@ -137,10 +123,10 @@ export class World3d extends Component {
             class="face topface"
             style="transform: translate3d(-19.5px, -170px, 0px) rotateX(-90deg) rotateY(0deg) rotateZ(0deg); width: 39px; height: 140px; background: rgb(74, 74, 74); border-radius: 2px"
           />
-          <div
+          {/*<div
             class="face bottomface"
             style="transform: translate3d(-19.5px, 30px, 0px) rotateX(90deg) rotateY(0deg) rotateZ(0deg); width: 39px; height: 140px; background: rgb(47, 47, 47);"
-          />
+          />*/}
         </div>
         <div
           class="box"
@@ -150,18 +136,18 @@ export class World3d extends Component {
             class="face frontface"
             style="transform: translate3d(-19.5px, -100px, 70px) rotateX(0deg) rotateY(0deg) rotateZ(0deg); width: 39px; height: 200px; background: rgb(75, 75, 75);"
           />
-          <div
+          {/*<div
             class="face backface"
             style="transform: translate3d(-19.5px, -100px, -70px) rotateX(0deg) rotateY(180deg) rotateZ(0deg); width: 39px; height: 200px; background: rgb(48, 48, 48);"
-          />
+          />*/}
           <div
             class="face rightface"
             style="transform: translate3d(-50.5px, -100px, 0px) rotateX(0deg) rotateY(-90deg) rotateZ(0deg); width: 140px; height: 200px; background: rgb(66, 66, 66);"
           />
-          <div
+          {/*<div
             class="face leftface"
             style="transform: translate3d(-89.5px, -100px, 0px) rotateX(0deg) rotateY(90deg) rotateZ(0deg); width: 140px; height: 200px; background: rgb(63, 63, 63);"
-          />
+          />*/}
           <div
             class="face topface"
             style="transform: translate3d(-19.5px, -170px, 0px) rotateX(-90deg) rotateY(0deg) rotateZ(0deg); width: 39px; height: 140px; background: rgb(87, 87, 87);"
@@ -179,18 +165,18 @@ export class World3d extends Component {
             class="face frontface"
             style="transform: translate3d(-19.5px, -100px, 70px) rotateX(0deg) rotateY(0deg) rotateZ(0deg); width: 39px; height: 200px; background: rgb(68, 68, 68);"
           />
-          <div
+          {/*<div
             class="face backface"
             style="transform: translate3d(-19.5px, -100px, -70px) rotateX(0deg) rotateY(180deg) rotateZ(0deg); width: 39px; height: 200px; background: rgb(54, 54, 54);"
-          />
+          />*/}
           <div
             class="face rightface"
             style="transform: translate3d(-50.5px, -100px, 0px) rotateX(0deg) rotateY(-90deg) rotateZ(0deg); width: 140px; height: 200px; background: rgb(63, 63, 63);"
           />
-          <div
+          {/*<div
             class="face leftface"
             style="transform: translate3d(-89.5px, -100px, 0px) rotateX(0deg) rotateY(90deg) rotateZ(0deg); width: 140px; height: 200px; background: rgb(62, 62, 62);"
-          />
+          />*/}
           <div
             class="face topface"
             style="transform: translate3d(-19.5px, -170px, 0px) rotateX(-90deg) rotateY(0deg) rotateZ(0deg); width: 39px; height: 140px; background: rgb(74, 74, 74);"
@@ -204,7 +190,7 @@ export class World3d extends Component {
           class="box"
           style="transform: translate3d(150px, 280px, -250px) rotateX(0deg) rotateY(-90deg) rotateZ(0deg)"
         >
-          <div
+          {/*<div
             class="face frontface"
             style="transform: translate3d(-19.5px, -100px, 70px) rotateX(0deg) rotateY(0deg) rotateZ(0deg); width: 39px; height: 200px; background: rgb(68, 68, 68);"
           />
@@ -219,24 +205,24 @@ export class World3d extends Component {
           <div
             class="face leftface"
             style="transform: translate3d(-89.5px, -100px, 0px) rotateX(0deg) rotateY(90deg) rotateZ(0deg); width: 140px; height: 200px; background: rgb(62, 62, 62);"
-          />
+          />*/}
           <div
             class="face topface"
             style="transform: translate3d(-19.5px, -170px, 0px) rotateX(-90deg) rotateY(0deg) rotateZ(0deg); width: 39px; height: 140px; background: rgb(74, 74, 74);"
           />
-          <div
+          {/*<div
             class="face bottomface"
             style="transform: translate3d(-19.5px, 30px, 0px) rotateX(90deg) rotateY(0deg) rotateZ(0deg); width: 39px; height: 140px; background: rgb(47, 47, 47);"
-          />
+          />*/}
         </div>
         <div
           class="box"
           style="transform: translate3d(150px, 280px, -250px) rotateX(0deg) rotateY(-120deg) rotateZ(0deg)"
         >
-          <div
+          {/*<div
             class="face frontface"
             style="transform: translate3d(-19.5px, -100px, 70px) rotateX(0deg) rotateY(0deg) rotateZ(0deg); width: 39px; height: 200px; background: rgb(68, 68, 68);"
-          />
+          />*/}
           <div
             class="face backface"
             style="transform: translate3d(-19.5px, -100px, -70px) rotateX(0deg) rotateY(180deg) rotateZ(0deg); width: 39px; height: 200px; background: rgb(54, 54, 54);"
@@ -245,10 +231,10 @@ export class World3d extends Component {
             class="face rightface"
             style="transform: translate3d(-50.5px, -100px, 0px) rotateX(0deg) rotateY(-90deg) rotateZ(0deg); width: 140px; height: 200px; background: rgb(63, 63, 63);"
           />
-          <div
+          {/*<div
             class="face leftface"
             style="transform: translate3d(-89.5px, -100px, 0px) rotateX(0deg) rotateY(90deg) rotateZ(0deg); width: 140px; height: 200px; background: rgb(62, 62, 62);"
-          />
+          />*/}
           <div
             class="face topface"
             style="transform: translate3d(-19.5px, -170px, 0px) rotateX(-90deg) rotateY(0deg) rotateZ(0deg); width: 39px; height: 140px; background: rgb(74, 74, 74);"
@@ -283,10 +269,10 @@ export class World3d extends Component {
               class="face topface"
               style="transform: translate3d(-11.5px, -135px, 0px) rotateX(-90deg) rotateY(0deg) rotateZ(0deg); width: 23px; height: 80px; background: rgb(204, 204, 231);"
             />
-            <div
+            {/*<div
               class="face bottomface"
               style="transform: translate3d(-11.5px, 55px, 0px) rotateX(90deg) rotateY(0deg) rotateZ(0deg); width: 23px; height: 80px; background: rgb(96, 96, 109);"
-            />
+            />*/}
           </div>
           <div
             class="box moving"
@@ -312,10 +298,10 @@ export class World3d extends Component {
               class="face topface"
               style="transform: translate3d(-11.5px, -135px, 0px) rotateX(-90deg) rotateY(0deg) rotateZ(0deg); width: 23px; height: 80px; background: rgb(204, 204, 231);"
             />
-            <div
+            {/*<div
               class="face bottomface"
               style="transform: translate3d(-11.5px, 55px, 0px) rotateX(90deg) rotateY(0deg) rotateZ(0deg); width: 23px; height: 80px; background: rgb(96, 96, 109);"
-            />
+            />*/}
           </div>
           <div
             class="box moving"
@@ -341,10 +327,10 @@ export class World3d extends Component {
               class="face topface"
               style="transform: translate3d(-11.5px, -135px, 0px) rotateX(-90deg) rotateY(0deg) rotateZ(0deg); width: 23px; height: 80px; background: rgb(204, 204, 231);"
             />
-            <div
+            {/*<div
               class="face bottomface"
               style="transform: translate3d(-11.5px, 55px, 0px) rotateX(90deg) rotateY(0deg) rotateZ(0deg); width: 23px; height: 80px; background: rgb(96, 96, 109);"
-            />
+            />*/}
           </div>
           <div
             class="box moving"
@@ -370,10 +356,10 @@ export class World3d extends Component {
               class="face topface"
               style="transform: translate3d(-11.5px, -135px, 0px) rotateX(-90deg) rotateY(0deg) rotateZ(0deg); width: 23px; height: 80px; background: rgb(204, 204, 231);"
             />
-            <div
+            {/*<div
               class="face bottomface"
               style="transform: translate3d(-11.5px, 55px, 0px) rotateX(90deg) rotateY(0deg) rotateZ(0deg); width: 23px; height: 80px; background: rgb(96, 96, 109);"
-            />
+            />*/}
           </div>
           <div
             class="box moving"
@@ -399,10 +385,10 @@ export class World3d extends Component {
               class="face topface"
               style="transform: translate3d(-11.5px, -135px, 0px) rotateX(-90deg) rotateY(0deg) rotateZ(0deg); width: 23px; height: 80px; background: rgb(231, 231, 255);"
             />
-            <div
+            {/*<div
               class="face bottomface"
               style="transform: translate3d(-11.5px, 55px, 0px) rotateX(90deg) rotateY(0deg) rotateZ(0deg); width: 23px; height: 80px; background: rgb(69, 69, 78);"
-            />
+            />*/}
           </div>
           <div
             class="box moving"
@@ -428,10 +414,10 @@ export class World3d extends Component {
               class="face topface"
               style="transform: translate3d(-11.5px, -135px, 0px) rotateX(-90deg) rotateY(0deg) rotateZ(0deg); width: 23px; height: 80px; background: rgb(204, 204, 231);"
             />
-            <div
+            {/*<div
               class="face bottomface"
               style="transform: translate3d(-11.5px, 55px, 0px) rotateX(90deg) rotateY(0deg) rotateZ(0deg); width: 23px; height: 80px; background: rgb(96, 96, 109);"
-            />
+            />*/}
           </div>
           <div
             class="box moving"
@@ -457,10 +443,10 @@ export class World3d extends Component {
               class="face topface"
               style="transform: translate3d(-11.5px, -135px, 0px) rotateX(-90deg) rotateY(0deg) rotateZ(0deg); width: 23px; height: 80px; background: rgb(204, 204, 231);"
             />
-            <div
+            {/*<div
               class="face bottomface"
               style="transform: translate3d(-11.5px, 55px, 0px) rotateX(90deg) rotateY(0deg) rotateZ(0deg); width: 23px; height: 80px; background: rgb(96, 96, 109);"
-            />
+            />*/}
           </div>
           <div
             class="box moving"
@@ -486,10 +472,10 @@ export class World3d extends Component {
               class="face topface"
               style="transform: translate3d(-11.5px, -135px, 0px) rotateX(-90deg) rotateY(0deg) rotateZ(0deg); width: 23px; height: 80px; background: rgb(204, 204, 231);"
             />
-            <div
+            {/*<div
               class="face bottomface"
               style="transform: translate3d(-11.5px, 55px, 0px) rotateX(90deg) rotateY(0deg) rotateZ(0deg); width: 23px; height: 80px; background: rgb(96, 96, 109);"
-            />
+            />*/}
           </div>
         </div>
       </div>
